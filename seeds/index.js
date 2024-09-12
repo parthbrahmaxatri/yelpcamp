5ee41b969e40710d4573df8bae1886ce12c451e5
Parth Brahmaxatri <parthbrahmaxatri007@gmail.com>
2024-09-12 17:06:16 +0530

    adding basic styles

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,14 @@ const seedDB = async () => {
 
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20 + 10);
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: `https://picsum.photos/400?random=${Math.random()}`,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum voluptatibus quasi perferendis alias iste corporis debitis voluptas totam, sed suscipit consectetur iure dolorem corrupti quae, facilis quidem minima asperiores nemo!",
+      price,
     });
     await camp.save();
   }
